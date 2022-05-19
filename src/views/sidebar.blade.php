1082a6599845cc61f65439b65c3b511e9ee2da88
Ahmed Bermawy <ahmed_bermawy@yahoo.com>
2022-05-19 13:09:24 +0200

    fix sidebar issue

--- src/views/sidebar.blade.php
+++ src/views/sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url('/dashboard/home') }}" class="brand-link">
-        <img src="{{ asset('img/dashboard/brand.png') }}" alt="Incubator" class="brand-image img-circle elevation-3"
+        <img src="{{ asset('img/dashboard/brand.png') }}" alt="Incubator" class="bg-light brand-image img-circle elevation-3"
              style="opacity: .8">
         <span class="brand-text font-weight-light">{{ 'Incubator' }}</span>
     </a>
@@ -29,25 +29,40 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-header">DASHBOARD</li>
                 <li
-                        class="nav-item {{ Request::segment(2) == 'backendproducts' ? 'menu-is-opening menu-open' : '' }}">
+                        class="nav-item {{ Request::segment(2) == 'admins' || Request::segment(2) == 'roles' || Request::segment(2) == 'permissions' ? 'menu-is-opening menu-open' : '' }}">
                     <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-store"></i>
+                        <i class="nav-icon fas fa-users-cog"></i>
                         <p>
-                            Products
+                            Administrator
                             <i class="fas fa-angle-left right"></i>
-                            {{--                            <i class="fas fa-store right"></i>--}}
-
-
                             <span class="badge badge-info right"></span>
                         </p>
                     </a>
                     <ul class="nav nav-treeview">
-                        @can('backendproduct-view')
+                        @can('admin-view')
                             <li class="nav-item">
-                                <a href="{{ url('/dashboard/backendproducts') }}"
-                                   class="nav-link {{ Request::segment(2) == 'backendproducts' ? 'active' : '' }}">
-                                    <i class="fab fa-product-hunt"></i>
-                                    <p> Products </p>
+                                <a href="{{ url('/dashboard/admins') }}"
+                                   class="nav-link {{ Request::segment(2) == 'admins' ? 'active' : '' }}">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p> Admins </p>
+                                </a>
+                            </li>
+                        @endcan
+                        @can('role-view')
+                            <li class="nav-item">
+                                <a href="{{ url('/dashboard/roles') }}"
+                                   class="nav-link {{ Request::segment(2) == 'roles' ? 'active' : '' }}">
+                                    <i class="fab fa-critical-role"></i>
+                                    <p> Roles </p>
+                                </a>
+                            </li>
+                        @endcan
+                        @can('permission-view')
+                            <li class="nav-item">
+                                <a href="{{ url('/dashboard/permissions') }}"
+                                   class="nav-link {{ Request::segment(2) == 'permissions' ? 'active' : '' }}">
+                                    <i class="fab fa-critical-role"></i>
+                                    <p> Permissions </p>
                                 </a>
                             </li>
                         @endcan
