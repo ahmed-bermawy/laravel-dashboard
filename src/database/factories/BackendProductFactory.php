ba125005491134e79cec7924a8e47df2d160bc27
Ahmed Bermawy <ahmed_bermawy@yahoo.com>
2022-05-30 12:08:54 +0200

    edit backend product factory to be compatible  with laravel 8 & 9

--- src/database/factories/BackendProductFactory.php
+++ src/database/factories/BackendProductFactory.php
@@ -1,22 +1,34 @@
 <?php
 
-use Faker\Generator as Faker;
-use Illuminate\Support\Str;
-use Carbon\Carbon;
-use Illuminate\Support\Facades\Hash;
+namespace Database\Factories\dashboard;
 
-$factory->define(\App\Models\dashboard\BackendProduct::User::class, function (Faker $faker) {
-    return [
-        'name' => $this->faker->sentence(4),
-        'short_description' => $this->faker->sentence(20),
-        'description' => $this->faker->realText(1000),
-        'image' => $this->faker->image,
-        'file' => '',
-        'group_id' => rand(1,2),
-        'free_delivery' => rand(0,1),
-        'release_date' => Carbon::now(),
-        'publish_date' => Carbon::now(),
-        'password' => Hash::make(Str::random(8)),
-        'property_id' => Str::random(8),
-    ];
-});
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class BackendProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->sentence(4),
+            'short_description' => $this->faker->sentence(20),
+            'description' => $this->faker->realText(1000),
+            'image' => $this->faker->image,
+            'file' => '',
+            'group_id' => rand(1,2),
+            'free_delivery' => rand(0,1),
+            'release_date' => Carbon::now(),
+            'publish_date' => Carbon::now(),
+            'password' => Hash::make(Str::random(8)),
+            'property_id' => Str::random(8),
+        ];
+    }
+}
